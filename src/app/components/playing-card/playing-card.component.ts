1b5561fe3fa1bf9a5fdcc2679409a0c654342e53
BlinkyCrazyDev <fredericcazesulfourt@yahoo.fr>
2025-01-30 15:40:38 +0100

    feat: initial commit

---
+++ src/app/components/playing-card/playing-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playing-card',
+  templateUrl: './playing-card.component.html',
+  styleUrls: ['./playing-card.component.css'],
+  standalone: true
+})
+export class PlayingCardComponent {
+
+}
